ee8fd1975e0cbd08b57923890c442378c0a9ed7b
Yeye <jose.mfa0609@hotmail.com>
2016-07-26 21:37:56 -0500

    Cree la tabla de movies y la de genre

---
+++ app/Genre.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BlogYeye;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Genre extends Model
+{
+    //
+}
---
+++ app/Movie.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BlogYeye;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    //
+}
